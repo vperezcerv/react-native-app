cca39fd413d58c6eb2bfdb48dccd283ef9d89c2f
joseuribe0624 <joseuribe0624@javerianacali.edu.co>
2023-11-27 10:40:35 -0500

    initial commit simple app

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react';
 
 export default function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(()=>{
+    getBio('basarat').then(setUser);
+  }, []);
+
+  async function getBio(username){
+    const url = `https://api.github.com/users/${username}`;
+
+    const res=await fetch(url);
+    const obj = await res.json();
+    return {name: obj.name, bio: obj.bio}; 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to this app that show github code</Text>
+      {/* <Button title="Get user"/> */}
+      <StatusBar style="auto" />{
+        <View>
+          <View style={styles.section}>
+            <Text style={styles.heading}>Name</Text>
+            <Text style={styles.paragraph}>{user.name}</Text>
+          </View>
+          <View>
+            <Text style={styles.heading}>Bio</Text>
+            <Text style={styles.paragraph}>{user.bio}</Text>
+          </View>
+        </View>
+      }
     </View>
   );
 }
